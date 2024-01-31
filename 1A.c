ded27db31ac1fa14a3bd41591e47ab0659c6a714
Arnob <farhanasfar21@gmail.com>
2024-02-01 05:29:42 +0600

    set sublime text to code

--- 1A.c
+++ 1A.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 int main()
 {
+    #ifndef a
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
     int n,m,a,x;
     scanf("%d%d%d",&n,&m,&a);
     x=(n/a)*(m/a);
     printf("%d\n",x);
 
+    printf("sublime text worked!");
 
     return 0;
 }
